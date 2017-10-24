36eda1025ac6618c7083ce3c1006359f536d3d4a
francois <francois@linux>
2017-10-24 19:05:12 +0200

    Adding TODO, LibYamlDB, .gitignore Correcting db.table.yaml

---
+++ LibYamlDB/cmake-build-debug/CMakeFiles/LibYamlDB.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LibYamlDB.dir/library.c.o"
+  "libLibYamlDB.pdb"
+  "libLibYamlDB.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/LibYamlDB.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
